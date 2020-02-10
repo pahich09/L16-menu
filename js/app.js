51ff0150cdb5cb35d2a3682cc02b2c59c27f73c6
Pavel <grebeniuk.p@gmail.com>
2020-02-10 13:47:33 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log('App was loaded...');
+
+const a = document.querySelectorAll('ul.main-menu a');
+const ul = document.querySelectorAll('ul.main-menu ul');
+
+[].map.call(a, el => el.addEventListener('click', event => {
+  event.preventDefault();
+  [...ul].map(el => el.classList.remove('open'));
+
+  const openParentsUl = () => {
+    console.log(event);
+    [].map.call((event.composedPath()).filter(el => el.tagName === 'UL').filter(el => el.matches('.main-menu ul')), el => {
+      el.classList.add('open');
+      el.nextElementSibling.classList.add('selected');
+    })
+  }
+  if (event.target.classList.contains('selected')) {
+    event.target.previousElementSibling ? event.target.classList.remove('selected') : null;
+  } else {
+    [...a].map(el => el.classList.remove('selected'));
+    event.target.classList.add('selected');
+    event.target.previousElementSibling ? event.target.previousElementSibling.classList.add('open') : null;
+  }
+  openParentsUl();
+}))
